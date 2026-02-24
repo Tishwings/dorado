b31eb5312074121d056371a1b7ed94d1d3a2f005
Ben Lawrence <ben.lawrence@nanoporetech.com>
2026-02-24 15:06:28 +0000

    [DOR-1233] Add first pass at benchmarks, again

---
+++ dorado/batchsize_benchmarks/batchsize_benchmarks.cpp
@@ -0,0 +1,262 @@
+#include "batchsize_benchmarks/batchsize_benchmarks.h"
+
+#include "BenchmarkCache.h"
+#include "BenchmarkSink.h"
+#include "api/runner_creation.h"
+#include "config/BasecallModelConfig.h"
+#include "data_loader/DataLoader.h"
+#include "read_pipeline/base/ReadPipeline.h"
+#include "read_pipeline/nodes/BasecallerNode.h"
+#include "spdlog/spdlog.h"
+#include "utils/jthread.h"
+#include "utils/parameters.h"
+#include "utils/string_utils.h"
+#include "utils/sys_utils.h"
+
+#include <c10/core/CachingDeviceAllocator.h>
+
+#include <algorithm>
+#include <chrono>
+#include <optional>
+#include <thread>
+
+#if DORADO_CUDA_BUILD
+#include "basecall/CudaCaller.h"
+
+#include <ATen/cuda/CUDAContextLight.h>
+#include <c10/cuda/CUDAGuard.h>
+#elif DORADO_METAL_BUILD
+#include "basecall/MetalCaller.h"
+#endif
+
+namespace dorado::batchsize_benchmarks {
+
+namespace {
+
+// Floating point version of chrono::seconds.
+using Seconds = std::chrono::duration<double>;
+
+Seconds min_benchmark_duration(const config::BasecallModelConfig &config) {
+    const bool is_fast = utils::contains(config.model_name(), "fast");
+    return is_fast ? Seconds(5) : Seconds(10);
+}
+
+SpeedEntry calculate_one(const std::string &device,
+                         const config::BasecallModelConfig &config,
+                         const data_loader::InputFiles &input_files) {
+    const auto batch_size = config.basecaller.batch_size();
+
+    // Create runners.
+    auto [runners, num_devices] = api::create_basecall_runners(
+            {
+                    .model_config = config,
+                    .device = device,
+                    .memory_limit_fraction = 1.f,
+                    .pipeline_type = api::PipelineType::simplex,
+                    .batch_size_time_penalty = 0.f,
+            },
+            utils::default_parameters.num_runners, 0);
+
+    // Reset peak allocator counters so that we can measure how much was used during processing.
+#if DORADO_CUDA_BUILD
+    const c10::Device torch_device{device};
+#elif DORADO_METAL_BUILD
+    const c10::Device torch_device{c10::DeviceType::MPS};  // TODO: should this be both MPS and CPU?
+#endif
+    auto *const allocator = c10::getDeviceAllocator(torch_device.type());
+    allocator->resetPeakStats(torch_device.index());
+
+    // Build a benchmarking pipeline.
+    PipelineDescriptor descriptor;
+    const NodeHandle sink_handle = descriptor.add_node<BenchmarkSink>({});
+    descriptor.add_node<BasecallerNode>({sink_handle}, std::move(runners),
+                                        config.basecaller.overlap(), config.model_name(), 1000,
+                                        "BasecallerNode", config.mean_qscore_start_pos);
+    auto pipeline = Pipeline::create(std::move(descriptor), nullptr);
+
+    // We need to feed in the data on a separate thread since it'll block.
+    auto source = utils::jthread([&] {
+        data_loader::DataLoader loader(*pipeline, device, 1, 0, std::nullopt, {});
+        loader.load_reads(input_files, ReadOrder::UNRESTRICTED);
+    });
+
+    // Wait for reads to start coming through.
+    auto &sink = pipeline->get_node_ref<BenchmarkSink>(sink_handle);
+    const Seconds first_read_latency = sink.wait_for_reads();
+    spdlog::debug("First read latency for batchsize={}: {}s", batch_size,
+                  first_read_latency.count());
+
+    // Make sure we let at least a few batches through before the benchmark ends.
+    const int min_num_batches = 5;
+    const auto benchmark_duration =
+            std::max(first_read_latency * min_num_batches, min_benchmark_duration(config));
+
+    // Let the benchmark run and grab the speed.
+    std::this_thread::sleep_for(benchmark_duration);
+    const double speed = sink.samples_per_second();
+    spdlog::debug("[{}] {} @ {}", device, batch_size, speed);
+
+    // Teardown the pipeline. This will teardown the source thread too.
+    pipeline->terminate({.fast = utils::AsyncQueueTerminateFast::Yes});
+    source.join();
+
+    const auto aggregate_idx = static_cast<uint64_t>(c10::CachingAllocator::StatType::AGGREGATE);
+    const auto memory_used =
+            allocator->getDeviceStats(torch_device.index()).allocated_bytes.at(aggregate_idx).peak;
+
+    return SpeedEntry{
+            .batch_size = static_cast<uint32_t>(batch_size),
+            .basecall_speed = sink.samples_per_second(),
+            .memory_used = static_cast<uint64_t>(memory_used),
+    };
+}
+
+std::string get_gpu_name(const std::string &device) {
+    if (device == "cpu") {
+        throw std::logic_error("Trying to calculate batchsize on CPU");
+    }
+
+#if DORADO_CUDA_BUILD
+    c10::cuda::CUDAGuard device_guard(device);
+    cudaDeviceProp *prop = at::cuda::getCurrentDeviceProperties();
+    return prop->name;
+
+#elif DORADO_METAL_BUILD
+    return dorado::utils::get_mtl_device()->name()->utf8String();
+
+#else
+#error "Invalid build"
+#endif
+}
+
+int get_batch_size_granularity(const config::BasecallModelConfig &config) {
+#if DORADO_CUDA_BUILD
+    return basecall::CudaCaller::get_batch_size_granularity(config);
+#elif DORADO_METAL_BUILD
+    return config.is_lstm_model() ? basecall::MetalLSTMCaller::get_batch_size_granularity()
+                                  : basecall::MetalTxCaller::get_batch_size_granularity();
+#else
+#error "Invalid build"
+#endif
+}
+
+uint64_t get_gpu_mem_limit(const std::string &device, float memory_limit_fraction) {
+#if DORADO_CUDA_BUILD
+    return basecall::CudaCaller::get_gpu_mem_limit(device, memory_limit_fraction);
+#elif DORADO_METAL_BUILD
+    std::ignore = device;
+    std::ignore = memory_limit_fraction;
+    // TODO
+    return 0;
+#else
+#error "Invalid build"
+#endif
+}
+
+}  // namespace
+
+std::optional<int> get(const std::string &device,
+                       float memory_limit_fraction,
+                       const config::BasecallModelConfig &config,
+                       float batch_size_time_penalty) {
+    const std::string gpu_name = get_gpu_name(device);
+    const auto model_name = config.model_name();
+    const auto memory_limit = get_gpu_mem_limit(device, memory_limit_fraction);
+
+    return BenchmarkCache::with_lock([&](BenchmarkCache::CacheProxy proxy) -> std::optional<int> {
+        auto speeds = proxy.get_timings(gpu_name, model_name);
+        if (speeds.empty()) {
+            spdlog::debug("No batch size timings for {}/{}", gpu_name, model_name);
+            return std::nullopt;
+        }
+
+        auto is_slower = [](const SpeedEntry &lhs, const SpeedEntry &rhs) {
+            return lhs.basecall_speed < rhs.basecall_speed;
+        };
+
+        // Look for the fastest batch size.
+        const auto fastest_iter = std::max_element(speeds.begin(), speeds.end(), is_slower);
+        if (fastest_iter == speeds.end()) {
+            throw std::logic_error("Failed to find fastest batch size in non-empty span");
+        }
+        spdlog::debug("Fastest batch size is {}", fastest_iter->batch_size);
+
+        // Cap to the largest batch size under our memory limit.
+        const auto first_batchsize_over_max_iter = std::find_if(
+                speeds.begin(), std::next(fastest_iter), [memory_limit](const SpeedEntry &entry) {
+                    return entry.memory_used > memory_limit;
+                });
+        if (first_batchsize_over_max_iter == speeds.begin()) {
+            throw std::runtime_error(
+                    fmt::format("No entries remaining after applying memory_limit_fraction ({})",
+                                memory_limit));
+        }
+
+        // Limit based on the time penalty.
+        const double threshold_speed =
+                fastest_iter->basecall_speed / (1.0 + batch_size_time_penalty);
+        auto fastest_under_threshold_iter =
+                std::find_if(speeds.begin(), first_batchsize_over_max_iter,
+                             [threshold_speed](const SpeedEntry &entry) {
+                                 return entry.basecall_speed >= threshold_speed;
+                             });
+        if (fastest_under_threshold_iter == speeds.end()) {
+            spdlog::debug(
+                    "No entries are faster than threshold of {}. Using absolute fastest instead",
+                    threshold_speed);
+            fastest_under_threshold_iter =
+                    std::max_element(speeds.begin(), first_batchsize_over_max_iter, is_slower);
+        }
+        if (fastest_under_threshold_iter == speeds.end()) {
+            throw std::logic_error("Error in batch size selection algorithm");
+        }
+        spdlog::debug("Fastest capped+limited batch size is {} @ {}",
+                      fastest_under_threshold_iter->batch_size,
+                      fastest_under_threshold_iter->basecall_speed);
+
+        return fastest_under_threshold_iter->batch_size;
+    });
+}
+
+void generate(const std::string &device,
+              const config::BasecallModelConfig &orig_config,
+              const data_loader::InputFiles &input_files) {
+    if (utils::running_in_docker()) {
+        spdlog::warn(
+                "Generating benchmarks inside of a container may not be representitive of the real "
+                "hardware.");
+    }
+
+    // Make a copy so that we can change the batch size.
+    auto config = orig_config;
+
+    const std::string gpu_name = get_gpu_name(device);
+    const auto batch_size_granularity = get_batch_size_granularity(config);
+
+    // Do the benchmarking.
+    spdlog::info("Benchmarking batch sizes in steps of {} for {} ({})", batch_size_granularity,
+                 device, gpu_name);
+    std::vector<SpeedEntry> speeds;
+    for (int batch_size = batch_size_granularity; true; batch_size += batch_size_granularity) {
+        config.basecaller.set_batch_size(batch_size);
+        const auto entry = calculate_one(device, config, input_files);
+        speeds.emplace_back(entry);
+    }
+
+    // Add them to the cache.
+    BenchmarkCache::with_lock([&](BenchmarkCache::CacheProxy proxy) {
+        proxy.add_timings(gpu_name, config.model_name(), std::move(speeds));
+    });
+}
+
+bool load_cache(const std::filesystem::path &file) {
+    return BenchmarkCache::with_lock(
+            [&](BenchmarkCache::CacheProxy proxy) { return proxy.load_from_file(file); });
+}
+
+bool export_cache(const std::filesystem::path &file) {
+    return BenchmarkCache::with_lock(
+            [&](BenchmarkCache::CacheProxy proxy) { return proxy.export_to_file(file); });
+}
+
+}  // namespace dorado::batchsize_benchmarks
--- dorado/batchsize_benchmarks/include/batchsize_benchmarks/batchsize_benchmarks.h
+++ dorado/batchsize_benchmarks/include/batchsize_benchmarks/batchsize_benchmarks.h
@@ -9,6 +9,10 @@ namespace dorado::config {
 struct BasecallModelConfig;
 }  // namespace dorado::config
 
+namespace dorado::data_loader {
+class InputFiles;
+}  // namespace dorado::data_loader
+
 namespace dorado::batchsize_benchmarks {
 
 // Retrieve the best batch size for a given config, if it exists in the cache.
@@ -20,9 +24,8 @@ std::optional<int> get(const std::string &device,
 // Generate full benchmarks for a given config on a device.
 // This will overwrite existing benchmarks if they exist.
 void generate(const std::string &device,
-              float memory_limit_fraction,
               const config::BasecallModelConfig &config,
-              const std::function<void(float)> &progress_callback);
+              const data_loader::InputFiles &input_files);
 
 // Load additional benchmarks into the cache.
 bool load_cache(const std::filesystem::path &file);
