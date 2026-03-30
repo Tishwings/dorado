264851b0dfd1444b4de57d178ca1e5729fdd0969
Ben Lawrence <ben.lawrence@nanoporetech.com>
2026-03-30 12:38:33 +0100

    [INSTX-13348] Trim dorado_tests_common's dependencies
    We only need main() and the TestUtils helpers so we don't need to
    depend on anything else. It'd be nice to remove the dependency on torch
    too, but that's not that simple so leave it for another day.

--- cmake/DoradoTarget.cmake
+++ cmake/DoradoTarget.cmake
@@ -50,6 +50,18 @@ function(dorado_add_library)
         list(APPEND targets ${test_name})
     endif()
 
+    # Validate that this target really doesn't link to torch.
+    # We defer this so that links not using this helper function aren't missed.
+    # Note that we don't check the tests since dorado_test_common links to torch.
+    if (arg_NO_TORCH)
+        cmake_language(EVAL CODE "
+            cmake_language(DEFER
+                DIRECTORY ${CMAKE_SOURCE_DIR}
+                CALL check_no_dependency_on_torch [[${arg_NAME}]]
+            )
+        ")
+    endif()
+
     foreach (target IN LISTS targets)
         # Add our dependencies.
         target_link_libraries(${target}
@@ -67,16 +79,7 @@ function(dorado_add_library)
         enable_warnings_as_errors(${target})
 
         # Reuse the PCH if it makes use of torch.
-        if (arg_NO_TORCH)
-            # Validate that this target really doesn't link to torch.
-            # We defer this so that links not using this helper function aren't missed.
-            cmake_language(EVAL CODE "
-                cmake_language(DEFER
-                    DIRECTORY ${CMAKE_SOURCE_DIR}
-                    CALL check_no_dependency_on_torch [[${target}]]
-                )
-            ")
-        elseif (DORADO_ENABLE_PCH)
+        if (DORADO_ENABLE_PCH AND NOT arg_NO_TORCH)
             get_target_property(type ${target} TYPE)
             if (type STREQUAL "EXECUTABLE")
                 target_precompile_headers(${target} REUSE_FROM dorado_pch_exe)
