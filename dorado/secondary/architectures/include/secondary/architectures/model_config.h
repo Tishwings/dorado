a597e43b973eec44cb156c4bf8c5537ee1616942
Ivan Sovic <ivan.sovic@nanoporetech.com>
2026-04-09 18:41:28 +0200

    [DOR-1518] Initializing members of ModelConfig.

--- dorado/secondary/architectures/include/secondary/architectures/model_config.h
+++ dorado/secondary/architectures/include/secondary/architectures/model_config.h
@@ -11,21 +11,21 @@ namespace dorado::secondary {
 
 struct ModelConfig {
     int32_t version = 0;
-    std::unordered_set<std::string> supported_basecallers;
+    std::unordered_set<std::string> supported_basecallers{};
 
     // Model section.
-    std::string model_type;
-    std::filesystem::path model_file;
-    std::filesystem::path model_dir;
-    std::unordered_map<std::string, std::string> model_kwargs;
+    std::string model_type{};
+    std::filesystem::path model_file{};
+    std::filesystem::path model_dir{};
+    std::unordered_map<std::string, std::string> model_kwargs{};
 
     // Feature encoder section.
-    std::string feature_encoder_type;
-    std::unordered_map<std::string, std::string> feature_encoder_kwargs;
-    std::vector<std::string> feature_encoder_dtypes;
+    std::string feature_encoder_type{};
+    std::unordered_map<std::string, std::string> feature_encoder_kwargs{};
+    std::vector<std::string> feature_encoder_dtypes{};
 
     // Label scheme section.
-    std::string label_scheme_type;
+    std::string label_scheme_type{};
 };
 
 ModelConfig parse_model_config(const std::filesystem::path& config_path,
