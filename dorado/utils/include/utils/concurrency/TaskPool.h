c94925e5bf219182c63a99c9889d396bdf6b707f
Ben Lawrence <ben.lawrence@nanoporetech.com>
2026-04-16 16:48:44 +0100

    [INSTX-13348] Allow mutable tasks to be executed
    If task() is mutable then so must the lambda holding it.

--- dorado/utils/include/utils/concurrency/TaskPool.h
+++ dorado/utils/include/utils/concurrency/TaskPool.h
@@ -48,7 +48,7 @@ public:
 
         // Keep track of the number of tasks in flight so that we can wait on them during a flush.
         counter.fetch_add(1, std::memory_order_relaxed);
-        q.try_push([task = std::forward<Func>(func), &counter] {
+        q.try_push([task = std::forward<Func>(func), &counter]() mutable {
             task();
 
             // We only need to wake waiters when this value hits 0, so avoid unnecessary notifies.
